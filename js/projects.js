4e9663ccfe2a37dcf51c8b6500a28df9e91c757b
nipun <nipungarg.nipun@gmail.com>
2021-06-29 03:38:50 +0530

    minor fixes in css

--- js/projects.js
+++ js/projects.js
@@ -1,54 +1,53 @@
 ///////////////////////////////////////////////// welcome div//////////////////////////////////////
-var letterP = document.querySelector('.letters.P')
-var letterR = document.querySelector('.letters.R')
-var letterO = document.querySelector('.letters.O')
-var letterJ = document.querySelector('.letters.J')
-var letterE1 = document.querySelector('.letters.E1')
-var letterC = document.querySelector('.letters.C')
-var letterT = document.querySelector('.letters.T')
-var letterS = document.querySelector('.letters.S')
+var letterP = document.querySelector(".letters.P");
+var letterR = document.querySelector(".letters.R");
+var letterO = document.querySelector(".letters.O");
+var letterJ = document.querySelector(".letters.J");
+var letterE1 = document.querySelector(".letters.E1");
+var letterC = document.querySelector(".letters.C");
+var letterT = document.querySelector(".letters.T");
+var letterS = document.querySelector(".letters.S");
 let count = 0;
 
 setTimeout(() => {
-    letterP.classList.add('animateShow')
-}, 600)
+  letterP.classList.add("animateShow");
+}, 600);
 setTimeout(() => {
-    letterR.classList.add('animateShow')
-}, 800)
+  letterR.classList.add("animateShow");
+}, 800);
 setTimeout(() => {
-    letterO.classList.add('animateShow')
-}, 1000)
+  letterO.classList.add("animateShow");
+}, 1000);
 setTimeout(() => {
-    letterJ.classList.add('animateShow')
-}, 1200)
+  letterJ.classList.add("animateShow");
+}, 1200);
 setTimeout(() => {
-    letterE1.classList.add('animateShow')
-}, 1400)
+  letterE1.classList.add("animateShow");
+}, 1400);
 setTimeout(() => {
-    letterC.classList.add('animateShow')
-}, 1600)
+  letterC.classList.add("animateShow");
+}, 1600);
 setTimeout(() => {
-    letterT.classList.add('animateShow')
-}, 1800)
+  letterT.classList.add("animateShow");
+}, 1800);
 setTimeout(() => {
-    letterS.classList.add('animateShow')
-}, 2000)
+  letterS.classList.add("animateShow");
+}, 2000);
 
-
-// number increase 
+// number increase
 var intervalId;
 letterS.addEventListener("webkitAnimationEnd", () => {
-    intervalId = setInterval(() => {
-        increaseCount();
-    }, 200);
+  intervalId = setInterval(() => {
+    increaseCount();
+  }, 200);
 });
 
 function increaseCount() {
-    count++;
-    if (count == 15) {
-        document.getElementById('countSpan').innerHTML = "15+";
-        clearInterval(intervalId);
-    } else {
-        document.getElementById('countSpan').innerHTML = " " + count;
-    }
+  count++;
+  if (count == 20) {
+    document.getElementById("countSpan").innerHTML = "15+";
+    clearInterval(intervalId);
+  } else {
+    document.getElementById("countSpan").innerHTML = " " + count;
+  }
 }
